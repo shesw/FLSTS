c209f849ea1912f85d467e27bbc159dde657f5b0
shesw <anjicsl14@gmail.com>
2018-08-11 19:14:31 +0800

    显示图片

--- pages/display/display.js
+++ pages/display/display.js
@@ -1,66 +1,42 @@
 // pages/display/display.js
+import request from '../../utils/request.js';
 Page({
+
+  searchKey: "",
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    pics: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  key(e) {
+    this.searchKey = e.detail.value;
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  btn() {
+    request.get('favorPics', {
+      key: this.searchKey
+    }).then((res) => {
+      this.setData({
+        pics: res.pics,
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  preview(e) {
+    const that = this;
+    wx.previewImage({
+      current: that.data.pics[e.currentTarget.dataset.index],
+      urls: that.data.pics,
+    })
   }
+
 })
